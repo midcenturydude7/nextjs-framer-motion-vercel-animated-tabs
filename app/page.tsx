bc0c6818c3bb0fcf1ad88225af9d64a5b8092d8a
midCenturyDude7 <mgriffes@gmail.com>
2024-03-23 21:02:33 -0700

    Set up plop to autogenerate components and route directories

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24"></div>
+    <div className="flex min-h-screen flex-col items-start justify-between p-24">
+      Stuff will go here
+    </div>
   );
 }
